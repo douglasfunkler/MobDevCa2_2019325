a4ad0f9a55e9e65be73695a8180bac60b0efaf93
douglasfunkler <douglasfunkler@gmail.com>
2020-04-26 19:22:28 +0000

    Added favorite option on episodes page and installed sqlite storage with its plugin

--- Breaking-Bad-Fandom/src/app/pages/episode-details/episode-details.page.ts
+++ Breaking-Bad-Fandom/src/app/pages/episode-details/episode-details.page.ts
@@ -1,15 +1,23 @@
+import { ApiService } from './../../services/api.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-episode-details',
-  templateUrl: './episode-details.page.html',
-  styleUrls: ['./episode-details.page.scss'],
+    selector: 'app-episode-details',
+    templateUrl: './episode-details.page.html',
+    styleUrls: ['./episode-details.page.scss'],
 })
+
 export class EpisodeDetailsPage implements OnInit {
+    episode: any;
+    episodeId = null;
 
-  constructor() { }
+    constructor(private activatedRoute: ActivatedRoute, private api: ApiService) { }
 
-  ngOnInit() {
-  }
-
+    ngOnInit() {
+        this.episodeId = this.activatedRoute.snapshot.paramMap.get('id');
+        this.api.getEpisode(this.episodeId).subscribe(res => {
+            this.episode = res[0];
+        })
+    }
 }
